918541d7a07b6ace5a438832b72f20ef96c77742
Antoine Ponthieu <antoineponthieu@hotmail.fr>
2025-06-03 16:58:37 +0300

    Réalisation du composant HostInfo

--- src/components/HostInfo.jsx
+++ src/components/HostInfo.jsx
@@ -1,6 +1,22 @@
-function HostInfo() {
+import starActive from '../assets/star_active.svg';
+import starInactive from '../assets/star_inactive.svg';
+
+function HostInfo({ host, rating }) {
+    const stars = Array.from({ length: 5 }, (_, i) =>
+        i < parseInt(rating) ? starActive : starInactive
+    );
+
     return (
         <div className="host-info">
+            <div className="host-header">
+                <p className="host-name">{host.name}</p>
+                <img className="host-picture" src={host.picture} alt={host.name} />
+            </div>
+            <div className="host-rating">
+                {stars.map((src, i) => (
+                    <img key={i} src={src} alt="star" className="star-icon" />
+                ))}
+            </div>
         </div>
     );
 }
--- src/pages/Housing.jsx
+++ src/pages/Housing.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import annonces from '../../annonces.json';
 import Carrousel from '../components/Carousel';
+import HostInfo from '../components/HostInfo';
 
 
 function Housing() {
@@ -10,8 +11,19 @@ function Housing() {
     return (
         <div>
             <Carrousel pictures={logement?.pictures || []} />
-            <h1 className='housing-title'>{logement?.title}</h1>
-            <p className='housing-location'>{logement?.location}</p>
+            <div className='housing-host-infos-container'>
+                <div className='housing-infos'>
+                    <h1 className='housing-title'>{logement?.title}</h1>
+                    <p className='housing-location'>{logement?.location}</p>
+                    <div className='tag-container'>
+                    </div>
+                </div>
+                <div className='host-info-container'>
+                    <HostInfo host={logement.host} rating={logement.rating} />
+                </div>
+            </div>
+            <div className='housing-dropdown-container'>
+            </div>
         </div>
     );
 }
